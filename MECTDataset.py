e825011ea84191dfffd131a2ef6c15a80bc6aaa8
mr@rsl_ubuntu <maxi.rohleder@fau.de>
2019-08-01 12:53:07 -0700

    build MECTDataset and material calculations

--- MECTDataset.py
+++ MECTDataset.py
@@ -20,25 +20,32 @@ class DectDataset(Dataset):
             root_dir (string): Directory with all the images.
             transform (callable, optional): Optional transform to be applied on a sample.
         """
+        self.X = 620 # todo sizes that from filename
+        self.Y = 480
+        self.numProjections = 200
+
+
         self.root_dir = root_dir
-        self.precompute=precompute
-        self.torchDump =os.path.join(root_dir, "torch")
+        self.precompute = precompute
+        self.torchDump = os.path.join(root_dir, "torch")
         self.device = device
         self.length = sum(os.path.isdir(os.path.join(root_dir, i)) and "_" in i for i in os.listdir(root_dir))
-        self.numProjections = 200
         self.transform = transform
         # detect number of samples
         self.samples = []
         for i in range(self.length):
             naming_scheme = "{}/*_{}/**/*.raw".format(root_dir, i)
             self.samples.append([n for n in glob.glob(naming_scheme, recursive=True)])
+        # Dual Energy implicates 2 energy projections (rest must be material)
+        self.numberOfMaterials = len(self.samples[0]) - 2
 
         if precompute:
             if not os.path.isdir(self.torchDump):
                 print("building a faster accessible version of the dataset")
                 os.makedirs(self.torchDump)
                 self.constructTorchDump(self.torchDump)
-            elif os.path.isdir(self.torchDump) and len(os.listdir(self.torchDump)) != self.length * 2 * self.numProjections:
+            elif os.path.isdir(self.torchDump) and len(
+                    os.listdir(self.torchDump)) != self.length * 2 * self.numProjections:
                 print("torchdump seems outdated.. deleting and rebuilding...")
                 shutil.rmtree(self.torchDump)
                 os.makedirs(self.torchDump)
@@ -51,12 +58,13 @@ class DectDataset(Dataset):
 
     def __getitem__(self, idx):
         if self.precompute:
-            return torch.load(self.torchDump+"/poly_{}.pt".format(idx)), torch.load(self.torchDump+"/mat_{}.pt".format(idx))
+            return torch.load(self.torchDump + "/poly_{}.pt".format(idx)), torch.load(
+                self.torchDump + "/mat_{}.pt".format(idx))
         else:
-            files = self.samples[int((idx/(self.numProjections*self.length))*4)]
+            files = self.samples[int((idx / (self.numProjections * self.length)) * 4)]
             poly, mat = self.readToNumpy(files)
-            mat = mat[:, idx%200, :, :].reshape(3, 480, 620)
-            poly = poly[:, idx%200, :, :].reshape(2, 480, 620)
+            mat = mat[:, idx % 200, :, :].reshape(3, self.Y, self.X)
+            poly = poly[:, idx % 200, :, :].reshape(2, self.Y, self.X)
 
             mat = torch.tensor(mat, device=self.device)
             poly = torch.tensor(poly, device=self.device)
@@ -65,24 +73,23 @@ class DectDataset(Dataset):
     def readToNumpy(self, files):
         '''
         reads in big endian 32 bit float raw files and outputs numpy data
-        :param files:
-        :return:
+        :param files: string array containing the paths to poly and mat files
+        :return: numpy arrays in format [C, P, Y, X] C = number of materials/energy projections
         '''
-        assert len(files) == 5
         dt = np.dtype('>f4')
-        poly = np.empty((2, 200, 480, 620))
-        mat = np.empty((3, 200, 480, 620))
+        poly = np.empty((2, 200, self.Y, self.X))
+        mat = np.empty((self.numberOfMaterials, 200, self.Y, self.X))
         mat_channel = 0
         for file in files:
             if 'POLY80' in file:
                 f = np.fromfile(file=file, dtype=dt).newbyteorder().byteswap()
-                poly[0] = f.reshape(200, 480, 620)
+                poly[0] = f.reshape(200, self.Y, self.X)
             elif 'POLY120' in file:
                 f = np.fromfile(file=file, dtype=dt).newbyteorder().byteswap()
-                poly[1] = f.reshape(200, 480, 620)
+                poly[1] = f.reshape(200, self.Y, self.X)
             elif 'MAT' in file:
                 f = np.fromfile(file=file, dtype=dt).newbyteorder().byteswap()
-                mat[mat_channel] = f.reshape(200, 480, 620)
+                mat[mat_channel] = f.reshape(200, self.Y, self.X)
                 mat_channel += 1
             else:
                 print("ERROR IN FILE STRUCTURE")
@@ -93,8 +100,8 @@ class DectDataset(Dataset):
         for i, sample in enumerate(tqdm(self.samples)):
             poly, mat = self.readToNumpy(sample)
             for p in range(self.numProjections):
-                matp = mat[:, p, :, :].reshape(3, 480, 620)
-                polyp = poly[:, p, :, :].reshape(2, 480, 620)
+                matp = mat[:, p, :, :].reshape(self.numberOfMaterials, self.Y, self.X)
+                polyp = poly[:, p, :, :].reshape(2, self.Y, self.X)
                 matp = torch.tensor(matp, device=self.device)
                 polyp = torch.tensor(polyp, device=self.device)
                 torch.save(matp, self.torchDump + "/mat_{}.pt".format((i * self.numProjections) + p))
@@ -103,15 +110,15 @@ class DectDataset(Dataset):
 
 if __name__ == "__main__":
 
-    #use_cuda = torch.cuda.is_available()
-    use_cuda = False
+    # use_cuda = torch.cuda.is_available()
+    use_cuda = True
     device = torch.device("cuda:0" if use_cuda else "cpu")
 
     dp = DectDataset("/home/mr/Documents/bachelor/data/simulation", precompute=False, device=device)
     dnp = DectDataset("/home/mr/Documents/bachelor/data/simulation", precompute=True, device=device)
 
-    set_pre = DataLoader(dp, batch_size=50, num_workers=10, shuffle=False)
-    set_np = DataLoader(dnp, batch_size=50, num_workers=10, shuffle=False)
+    set_pre = DataLoader(dp, batch_size=2, num_workers=5, shuffle=False)
+    set_np = DataLoader(dnp, batch_size=2, num_workers=5, shuffle=False)
 
     start = time.time()
     for x, y in tqdm(set_pre):
@@ -125,4 +132,4 @@ if __name__ == "__main__":
         pass
     d2 = time.time() - start
 
-    print("pre: {}\nnpre: {}\nratio: {}".format(d1, d2, d1/d2))
+    print("pre: {}\nnpre: {}\nratio: {}".format(d1, d2, d1 / d2))
---
+++ material.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+
+elements = np.array(['H', 'O', 'C', 'N', 'Cl', 'Ca', 'I', 'Si', 'B', 'Na', 'Mg', 'Fe'])
+
+SolidWater =  np.array([0.0841, 0.1849, 0.6697, 0.0216, 0.0013, 0.0143, 0.0000, 0.0108, 0.0005, 0.0017, 0.0110, 0.0000])
+Iodine5mg =   np.array([0.0837, 0.1839, 0.6668, 0.0214, 0.0013, 0.0142, 0.0050, 0.0107, 0.0005, 0.0017, 0.0110, 0.0000])
+Iodine10mg =  np.array([0.0832, 0.1824, 0.6643, 0.0212, 0.0013, 0.0140, 0.0099, 0.0106, 0.0005, 0.0017, 0.0109, 0.0000])
+Iodine15mg =  np.array([0.0827, 0.1809, 0.6618, 0.0211, 0.0013, 0.0139, 0.0148, 0.0105, 0.0005, 0.0017, 0.0108, 0.0000])
+
+print("should be 1.0: " + str(SolidWater.sum()))
+print("should be 1.0: " + str(Iodine5mg.sum()))
+print("should be 1.0: " + str(Iodine10mg.sum()))
+print("should be 1.0: " + str(Iodine15mg.sum()))
+# ASSUMPTION: iodineXmg is based on solid water with mixed in iodine.
+# extrapolation of iodine concentration should verify that
+# using euclidian distance (l2) as measure of similarity
+
+# 1. calculate delta vector
+deltaIod5mg = Iodine5mg - SolidWater
+
+# 2. calculating errors of extrapolation
+extraIod10mg = SolidWater + 2 * deltaIod5mg
+extraIod15mg = SolidWater + 3 * deltaIod5mg
+e10mg = np.sqrt(np.sum(np.square(np.subtract(Iodine10mg, extraIod10mg)), axis=0))
+e15mg = np.sqrt(np.sum(np.square(np.subtract(Iodine15mg, extraIod15mg)), axis=0))
+print('Differences in Iod10mg (percent): \n{}\n{}'.format(elements, np.subtract(Iodine10mg, extraIod10mg)))
+print('Differences in Iod15mg (percent): \n{}\n{}'.format(elements, np.subtract(Iodine15mg, extraIod15mg)))
+
+print("should be 1.0: " + str(extraIod10mg.sum()))
+print("should be 1.0: " + str(extraIod15mg.sum()))
+
+print("distance from extrapolation to real Iod10mg: " + str(e10mg))
+print("distance from extrapolation to real Iod15mg:  " + str(e15mg))
+# 3. compose calcium of water plus iodine
+calcium50mg = 0
+
